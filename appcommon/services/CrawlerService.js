bc3dcad1f342ee7114cc4d9733d39b22d1c354c5
LocNT <locnt@vn.ibm.com>
2016-01-05 12:32:41 +0700

    execute increase

--- appcommon/services/CrawlerService.js
+++ appcommon/services/CrawlerService.js
@@ -209,12 +209,12 @@ var reCrawlerData = function(){
         getMaxIdLocal().then(function (dataLocal) {
             local = dataLocal[0].MaxLocal ? dataLocal[0].MaxLocal : 0;
             console.log("local : " + local);
-            if(local + 50 >= online > local){
+            if(local + 30 >= online > local){
                 console.log("------" + (local + 1) +"---" + online + "-----");
                 crawRangId(local + 1, online);
-            }else if(online > local + 50){
-                console.log("------" + (local + 1) +"---" + (local + 50) + "-----");
-                crawRangId(local + 1, local + 50);
+            }else if(online > local + 30){
+                console.log("------" + (local + 1) +"---" + (local + 30) + "-----");
+                crawRangId(local + 1, local + 30);
             }
 
         }, function (err) {
